99af1fec21aa6d6587d896282b6936127a1650b9
Stepan Petrosian <stephenpetrosian@gmail.com>
2019-10-29 18:55:34 +0300

    Add string_comparing.py and unflat.py

---
+++ string_comparing.py
@@ -0,0 +1,11 @@
+test_list = ['hello, world', 'hello, earth']
+
+
+def string_comparing(first_string, second_string):
+    first_string = first_string.split(',')
+    second_string = second_string.split(',')
+    answer = [word_1 for word_1 in first_string for word_2 in second_string if word_1 == word_2]
+    return answer
+
+
+print(lst_comparing(test_list[0], test_list[1]))
